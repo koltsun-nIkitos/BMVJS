e30e62ed60b9a8b55be33b13394fd3b756376044
Koltsun Nikita <koltsun-nv@yandex.ru>
2023-10-15 15:41:26 +0300

    v2.0.0

---
+++ js/smoothScroll.js
@@ -0,0 +1,15 @@
+const smoothScrollElems = document.querySelectorAll('a[href^="#"]:not(a[href="#"])');
+
+smoothScrollElems.forEach(link => {
+    link.addEventListener("click", (event) => {
+        event.preventDefault();
+
+        const id = link.getAttribute('href').substring(1);
+        document.getElementById(id).scrollIntoView(
+            {
+                behavior: 'smooth',
+            }
+        );
+
+    });
+})
---
+++ js/tabs.js
@@ -0,0 +1,22 @@
+const tabsHandlerElems = document.querySelectorAll('[data-tabs-handler]');
+const tabsFieldElems = document.querySelectorAll('[data-tabs-field]');
+
+for (const tab of tabsHandlerElems) {
+    tab.addEventListener('click', () => {
+        tabsHandlerElems.forEach((item) => {
+            if (tab === item){
+                item.classList.add('design-list__item_active');
+            } else {
+                item.classList.remove('design-list__item_active');
+            }
+        });
+
+        tabsFieldElems.forEach(item => {
+            if (item.dataset.tabsField === tab.dataset.tabsHandler) {
+                item.classList.remove("hidden");
+            } else {
+                item.classList.add('hidden');
+            }
+        });
+    })
+}
